df3895ae439a035b66db9875d424af5f9d7cb3e5
elerbec82 <56617596+elerbec82@users.noreply.github.com>
2019-10-15 21:20:03 -0500

    Add files via upload

---
+++ area rectangulo.py
@@ -0,0 +1,56 @@
+Base = int(input ("ingrese Base: "))
+Altura = int(input ("ingrese Altura: "))
+
+
+area = Base * Altura
+
+
+
+print("El area del rectandulo es =" + str (area))
+
+
+
+
+
+
+
+
+
+print( 8 % 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
